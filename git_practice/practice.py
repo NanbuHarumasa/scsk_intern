a8cc426513eb18d0b2169fdee21e163fdcf5ada4
taisei <taisei.mrchil@gmail.com>
2022-10-09 00:36:38 +0900

    say_goodbye関数を追加

--- git_practice/practice.py
+++ git_practice/practice.py
@@ -1,13 +1,16 @@
 def say_hello(name):
   print(f"Hello, {name}.")
 
+def say_goodbye(name):
+  print(f"Goodbye, {name}.")
+
 if __name__ == '__main__':
 
   # ここをみんなで書き換えてpushする
 
 
   your_name = "goto_yusaku"
-  your_name ="scsk" 
+  your_name ="scsk"
   your_name = "Taisei Fujisawa"
   your_name = "Yuya Koyama"
   your_name = "Honoka Yano"
@@ -16,3 +19,4 @@ if __name__ == '__main__':
 
 
   say_hello(your_name)
+  say_goodbye(your_name)
